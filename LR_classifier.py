47791a1a34c69d6822b86c9825e3f60171720da0
Yasaman-Mah <yassaman@bitbucket.org>
2018-01-10 16:03:33 -0500

    cross tests.

--- LR_classifier.py
+++ LR_classifier.py
@@ -11,27 +11,40 @@ train_healthy = np.load(os.path.join(BASE_DIR, "train_healthy.npy"))
 test_sick = np.load(os.path.join(BASE_DIR, "test_sick.npy"))
 train_sick = np.load(os.path.join(BASE_DIR, "train_sick.npy"))
 
-train_data = np.concatenate((train_healthy, train_sick))
-train_label = np.concatenate((np.zeros(train_healthy.shape[0]), np.ones(train_sick.shape[0])))
-#plt.figure()
-#plt.plot(train_sick[:,0], train_sick[:,1], 'ro')
-#plt.plot(train_healthy[:,0], train_healthy[:,1], 'k+')
-#plt.show()
-train_data = train_data.reshape(-1, 1)
+healthy = np.concatenate((train_healthy, test_healthy))
+sick = np.concatenate((train_sick, test_sick))
+all_scores = np.concatenate((healthy, sick))
+all_labels = np.concatenate((np.zeros(healthy.shape[0]), np.ones(sick.shape[0])))
+all_scores = all_scores.reshape(-1,1)
+all_labels = all_labels.reshape(-1, 1)
+all_data = np.concatenate((all_scores, all_labels), axis=1)
 
-test_data = np.concatenate((test_healthy, test_sick))
-test_label = np.concatenate((np.zeros(test_healthy.shape[0]), np.ones(test_sick.shape[0])))
-test_data = test_data.reshape(-1, 1)
+test_ratio = 0.2
+test_num = int((test_ratio * all_data.shape[0])//1)
+num_runs = 10
+error = np.zeros(num_runs)
 
-#print("train_data", train_data, "train labels", train_label) 
+for i in range(10):
+    np.random.shuffle(all_data)
+    test_data = all_data[:test_num]
+    train_data = all_data[test_num:]
+    svm = SVC()
+    svm.fit(train_data[:,0].reshape(-1, 1), train_data[:,1].ravel())
+    score = svm.score(test_data[:,0].reshape(-1,1), test_data[:,1].ravel())
+    error[i] = score
 
-svm = SVC()
-svm.fit(train_data, train_label.ravel())
 
-score = svm.score(test_data, test_label.ravel())
+fig1 = plt.figure()
+ax = fig1.add_subplot(111)
+ax.plot(np.arange(num_runs), error, 'ro')
+ax.plot(1, error.mean(), 'k+')
+ax.plot(1, np.median(error), 'bs')
+ax.legend(["error", "mean", "median"])
+fig1.suptitle("Error for different partitionings of dataset")
 
+fig1.savefig("error.png")
 
-print(score)
+print(error)
 
 
 
