672e44a37c9d562e4d50dc3eef73d05c391b30d5
Yasaman-Mah <yasmah95@yahoo.com>
2018-04-11 16:19:14 -0400

    based on previous data.

--- LR_classifier.py
+++ LR_classifier.py
@@ -1,53 +1,89 @@
 import numpy as np
 import os.path
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
-BASE_DIR = '/home/yasaman/Documents/Winter18/fluorescence/immunofluorescence_PCD/'
+BASE_DIR = '/home/yasaman/Mennella_Lab/immunofluorescence_PCD/'
 
 test_healthy = np.load(os.path.join(BASE_DIR, "test_healthy.npy"))
-train_healthy = np.load(os.path.join(BASE_DIR, "train_healthy.npy"))
+#train_healthy = np.load(os.path.join(BASE_DIR, "train_healthy.npy"))
 test_sick = np.load(os.path.join(BASE_DIR, "test_sick.npy"))
-train_sick = np.load(os.path.join(BASE_DIR, "train_sick.npy"))
+test_names = np.load(os.path.join(BASE_DIR, "test_names_hs.npy"))
+#train_sick = np.load(os.path.join(BASE_DIR, "train_sick.npy"))
 
-healthy = np.concatenate((train_healthy, test_healthy))
-sick = np.concatenate((train_sick, test_sick))
-all_scores = np.concatenate((healthy, sick))
-all_labels = np.concatenate((np.zeros(healthy.shape[0]), np.ones(sick.shape[0])))
-all_scores = all_scores.reshape(-1,1)
-all_labels = all_labels.reshape(-1, 1)
-all_data = np.concatenate((all_scores, all_labels), axis=1)
+#train = np.concatenate((train_healthy, train_sick))
+test = np.concatenate((test_healthy, test_sick))
+#all_scores = np.concatenate((healthy, sick))
+#all_labels = np.concatenate((np.zeros(healthy.shape[0]), np.ones(sick.shape[0])))
+#all_scores = all_scores.reshape(-1,1)
+#all_labels = all_labels.reshape(-1, 1)
+#all_data = np.concatenate((all_scores, all_labels), axis=1)
 
+#train_labels = np.concatenate((np.zeros(train_healthy.shape[0]), np.ones(train_sick.shape[0])))
+test = test.reshape(-1, 1)
+test_labels = np.concatenate((np.zeros(test_healthy.shape[0]), np.ones(test_sick.shape[0])))
+test_labels = test_labels.reshape(-1, 1)
+test_names = test_names.reshape(-1, 1)
+all_test = np.concatenate((test, test_labels, test_names), axis=1)
+
+'''
+svm = SVC()
+#lr = LogisticRegression()
+svm.fit(train.reshape(-1,1), train_labels.ravel())
+
+
+score = svm.score(test.reshape(-1,1), test_labels.ravel())
+predict = svm.predict(test.reshape(-1,1))
+confuse = confusion_matrix(test_labels.ravel(), predict, labels=[0, 1])
+print(score, confuse)
+
+plt.figure()
+plt.plot(train, train_labels, 'ro')
+plt.plot(test, test_labels, 'b+')
+plt.savefig("data_dist.pdf")
+'''
 test_ratio = 0.2
-test_num = int((test_ratio * all_data.shape[0])//1)
+test_num = int((test_ratio * all_test.shape[0])//1)
 num_runs = 10
 error = np.zeros(num_runs)
+confusion = np.zeros([num_runs, 2, 2])
 
-for i in range(10):
-    np.random.shuffle(all_data)
-    test_data = all_data[:test_num]
-    train_data = all_data[test_num:]
+
+
+for i in range(1):
+    np.random.shuffle(all_test)
+    test_data = all_test[:test_num]
+    train_data = all_test[test_num:]
     svm = SVC()
     svm.fit(train_data[:,0].reshape(-1, 1), train_data[:,1].ravel())
     score = svm.score(test_data[:,0].reshape(-1,1), test_data[:,1].ravel())
     error[i] = score
+    predict = svm.predict(test_data[:,0].reshape(-1, 1))
+    print(test_data[(test_data[:,1] != predict), 2])
+    confusion[i] = confusion_matrix(test_data[:,1], predict, labels=['0.0', '1.0'])
+    
+
 
 
 fig1 = plt.figure()
+
 ax = fig1.add_subplot(111)
-ax.plot(np.arange(num_runs), error, 'ro')
-ax.plot(1, error.mean(), 'k+')
-ax.plot(1, np.median(error), 'bs')
-ax.legend(["error", "mean", "median"])
+ax.hist(error)
+ax.axvline(error.mean(), color='b', linestyle='dashed', linewidth=2)
+ax.axvline(np.median(error), color='k', linestyle='dotted', linewidth=2)
+ax.legend(["mean", "median"])
 fig1.suptitle("Error for different partitionings of dataset")
 
-fig1.savefig("error.png")
+fig1.savefig("error2.pdf")
 
-print(error)
-
-
-
+avg_confusion = confusion.mean(axis=0)
+avg_confusion = avg_confusion/test_num
+print(avg_confusion)
 
 
 
--- read_data.py
+++ read_data.py
@@ -6,16 +6,16 @@ import matplotlib.pyplot as plt
 from scipy.stats.stats import pearsonr   
 
 # Data PATH
-BASE_DIR = '/home/yasaman/Documents/Winter18/fluorescence/'
-HEALTH_DIR ='healthy/'
-SICK_DIR = 'sick/'
+BASE_DIR = '/home/yasaman/Mennella_Lab/SPINNING_DISC_1_23_2018/'
+HEALTH_DIR ='healthy21/'
+SICK_DIR = 'sick21/'
 
 
 def process_image(im):
     ''' im is Image object, returned by call to open (Pillow) '''
     im_arr = np.asarray(im, dtype='float32')
     #im_arr = im_arr - im_arr.mean(axis=0).mean(axis=0)
-    thresh = im_arr[:,:,0] > np.percentile(im_arr[:,:,0], 20)
+    thresh = im_arr[:,:,0] > np.percentile(im_arr[:,:,0], 60)
     #diff = im_arr[thresh, 0] - im_arr[thresh, 1]
     corr, _ = pearsonr(im_arr[thresh, 0], im_arr[thresh, 1])
     return corr
@@ -24,41 +24,34 @@ healthy = os.listdir(os.path.join(BASE_DIR, HEALTH_DIR))
 sick = os.listdir(os.path.join(BASE_DIR, SICK_DIR))
 heal_avg_diff = []
 sic_avg_diff = []
+heal_sick_names =[]
 
 for im in healthy:
     healthy_im = Image.open(os.path.join(BASE_DIR, HEALTH_DIR, im))
     diff_heal = process_image(healthy_im)
     heal_avg_diff.append(diff_heal)
+    heal_sick_names.append(HEALTH_DIR + im)
     
 for im in sick:
     sick_im = Image.open(os.path.join(BASE_DIR, SICK_DIR, im))
     diff_sic = process_image(sick_im)
     sic_avg_diff.append(diff_sic)
+    heal_sick_names.append(SICK_DIR + im)
 
 heal_avg = np.asarray(heal_avg_diff)
 sic_avg = np.asarray(sic_avg_diff)
-
+hs_names = np.asarray(heal_sick_names)
 
 ##
-# shuffle and set aside a test set
-test_ratio = 0.2
-heal_test_num = int((test_ratio * heal_avg.shape[0])//1)
-sic_test_num = int((test_ratio * sic_avg.shape[0])//1)
 
-np.random.shuffle(heal_avg)
-np.random.shuffle(sic_avg)
-
-test_healthy = heal_avg[:heal_test_num]
-train_healthy = heal_avg[heal_test_num:]
-test_sick = sic_avg[:sic_test_num]
-train_sick = sic_avg[sic_test_num:]
+#np.random.shuffle(heal_avg)
+#np.random.shuffle(sic_avg)
 
 
-np.save("test_healthy", test_healthy)
-np.save("train_healthy", train_healthy)
-np.save("test_sick", test_sick)
-np.save("train_sick", train_sick)
 
+np.save("test_healthy", heal_avg)
+np.save("test_sick", sic_avg)
+np.save("test_names_hs", hs_names)
 '''
 fig1 = plt.figure()
 ax11 = fig1.add_subplot(121)
