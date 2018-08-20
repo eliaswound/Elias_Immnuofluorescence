473457a63bdc03fb9bc872d70e74e21c835b40cb
Yasaman-Mah <yasmah95@yahoo.com>
2018-08-20 14:13:20 -0400

    using new high res data, and bootstrapping.

--- LR_classifier.py
+++ LR_classifier.py
@@ -3,88 +3,54 @@ import os.path
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
-#import matplotlib
-#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
-BASE_DIR = '/home/yasaman/Mennella_Lab/immunofluorescence_PCD/'
+BASE_DIR = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/immunofluorescence_PCD/'
 
-test_healthy = np.load(os.path.join(BASE_DIR, "test_healthy.npy"))
-train_healthy = np.load(os.path.join(BASE_DIR, "train_healthy.npy"))
-test_sick = np.load(os.path.join(BASE_DIR, "test_sick.npy"))
-train_sick = np.load(os.path.join(BASE_DIR, "train_sick.npy"))
+wt = np.load(os.path.join(BASE_DIR, "wt.npy"))
+pcd35 = np.load(os.path.join(BASE_DIR, "pcd35.npy"))
+pcd36 = np.load(os.path.join(BASE_DIR, "pcd36.npy"))
+pcd38 = np.load(os.path.join(BASE_DIR, "pcd38.npy"))
 
-train = np.concatenate((train_healthy, train_sick))
-test = np.concatenate((test_healthy, test_sick))
-#all_scores = np.concatenate((healthy, sick))
-#all_labels = np.concatenate((np.zeros(healthy.shape[0]), np.ones(sick.shape[0])))
-#all_scores = all_scores.reshape(-1,1)
-#all_labels = all_labels.reshape(-1, 1)
-#all_data = np.concatenate((all_scores, all_labels), axis=1)
-
-train_labels = np.concatenate((np.zeros(train_healthy.shape[0]), np.ones(train_sick.shape[0])))
-test = test.reshape(-1, 1)
-test_labels = np.concatenate((np.zeros(test_healthy.shape[0]), np.ones(test_sick.shape[0])))
-test_labels = test_labels.reshape(-1, 1)
-#test_names = test_names.reshape(-1, 1)
-all_test = np.concatenate((test, test_labels), axis=1)
+#bootstrapping, 100 rounds
+# keep track of number of classified positives in each round
+predict_wt = np.zeros(100)
+predict_35 = np.zeros(100)
+predict_36 = np.zeros(100)
+predict_38 = np.zeros(100)
 
 
-svm = SVC()
-#lr = LogisticRegression()
-svm.fit(train.reshape(-1,1), train_labels.ravel())
+for i in range(100):
 
-score = svm.score(test.reshape(-1,1), test_labels.ravel())
-predict = svm.predict(test.reshape(-1,1))
-confuse = confusion_matrix(test_labels.ravel(), predict, labels=[0, 1])
-print(score, confuse)
-
-print("test size", test_labels.shape, "total pcd", test_labels.sum())
-plt.figure()
-plt.plot(train, train_labels, 'ro')
-plt.plot(test, test_labels, 'b+')
-#plt.savefig("data_dist.pdf")
-plt.show()
-
-'''
-test_ratio = 0.2
-test_num = int((test_ratio * all_test.shape[0])//1)
-num_runs = 10
-error = np.zeros(num_runs)
-confusion = np.zeros([num_runs, 2, 2])
+	# pick 200 random wildtypes for training
+	np.random.shuffle(wt)
+	train_healthy = wt[:200]
+	test_wt = wt[200:]
+	np.random.shuffle(pcd36)
+	train_sick = pcd36[:200]
+	test_36 = pcd36[200:]
 
 
 
-for i in range(1):
-    np.random.shuffle(all_test)
-    test_data = all_test[:test_num]
-    train_data = all_test[test_num:]
-    svm = SVC()
-    svm.fit(train_data[:,0].reshape(-1, 1), train_data[:,1].ravel())
-    score = svm.score(test_data[:,0].reshape(-1,1), test_data[:,1].ravel())
-    error[i] = score
-    predict = svm.predict(test_data[:,0].reshape(-1, 1))
-    print(test_data[(test_data[:,1] != predict), 2])
-    confusion[i] = confusion_matrix(test_data[:,1], predict, labels=['0.0', '1.0'])
-    
+	train = np.concatenate((train_healthy, train_sick))
+
+	train_labels = np.concatenate((np.zeros(train_healthy.shape[0]), np.ones(train_sick.shape[0])))
 
 
+	svm = SVC()
+	svm.fit(train.reshape(-1,1), train_labels.ravel())
 
-fig1 = plt.figure()
+	#train_pred = svm.predict(train.reshape(-1,1))
+	predict_wt[i] = svm.predict(test_wt.reshape(-1,1)).mean()
+	predict_35[i] = svm.predict(pcd35.reshape(-1,1)).mean()
+	predict_36[i] = svm.predict(test_36.reshape(-1,1)).mean()
+	predict_38[i] = svm.predict(pcd38.reshape(-1,1)).mean()
 
-ax = fig1.add_subplot(111)
-ax.hist(error)
-ax.axvline(error.mean(), color='b', linestyle='dashed', linewidth=2)
-ax.axvline(np.median(error), color='k', linestyle='dotted', linewidth=2)
-ax.legend(["mean", "median"])
-fig1.suptitle("Error for different partitionings of dataset")
-
-fig1.savefig("error2.pdf")
-
-avg_confusion = confusion.mean(axis=0)
-avg_confusion = avg_confusion/test_num
-print(avg_confusion)
-
-'''
+#confuse = confusion_matrix(train_labels.ravel(), train_pred, labels=[0, 1])
+#print("train confuse", confuse)
+print("wt ",predict_wt.mean(), np.percentile(predict_wt, 5), np.percentile(predict_wt, 95))
+print("35 ",predict_35.mean(), np.percentile(predict_35, 5), np.percentile(predict_35, 95))
+print("36 ",predict_36.mean() ,np.percentile(predict_36, 5), np.percentile(predict_36, 95))
+print("38 ",predict_38.mean(), np.percentile(predict_38, 5), np.percentile(predict_38, 95))
 
--- read_data.py
+++ read_data.py
@@ -4,16 +4,8 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats.stats import pearsonr   
+from scipy.ndimage.filters import gaussian_filter
 
-# Data PATH
-BASE_DIR = '/home/yasaman/Mennella_Lab/SPINNING_DISC_1_23_2018/'
-HEALTH_DIR ='healthy21/'
-SICK_DIR = 'sick21/'
-
-# test data path
-TEST_BASE = '/home/yasaman/Mennella_Lab/spin_disc2/'
-TEST_HEALTH = ['healthy_3_28', 'healthy_3_26']
-TEST_SICK = ['pcd22_04_03', 'pcd22_3_30']
 
 def process_image(im):
 	''' im is Image object, returned by call to open (Pillow) '''
@@ -39,40 +31,26 @@ def get_corr_im_dir(DIR):
 
 
 if (__name__ == "__main__"):
-	spin_sick = ['/home/yasaman/Mennella_Lab/SPINNING_DISC_1_23_2018/sick21/',
-		'/home/yasaman/Mennella_Lab/spin_disc2/pcd22_04_03',
-		'/home/yasaman/Mennella_Lab/spin_disc2/pcd22_3_30']
-	spin_healthy = ['/home/yasaman/Mennella_Lab/spin_disc2/healthy_3_26',
-		'/home/yasaman/Mennella_Lab/spin_disc2/healthy_3_28',
-		'/home/yasaman/Mennella_Lab/SPINNING_DISC_1_23_2018/sick21/']
 
-	wide_sick = '/home/yasaman/Mennella_Lab/WIDEFIELD_08_01_2018/PATIENT'
-	wide_healthy ='/home/yasaman/Mennella_Lab/WIDEFIELD_08_01_2018/HEALTHY'
 
-	wide_sick_cor, _ = get_corr_im_dir(wide_sick)
-	wide_heal_cor, _ = get_corr_im_dir(wide_healthy)
+	test_wt = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/WT_cropped'
+	pcd35 = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/PCD35_cropped'
+	pcd36 = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/PCD36_cropped'
+	pcd38 = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/PCD38_cropped'
 	
-	spin_sick_cor = []
-	spin_heal_cor = []
-	for dirc in spin_sick:
-		corrs, _ = get_corr_im_dir(dirc)
-		spin_sick_cor.append(corrs)
-	spin_sick_cor = np.concatenate(spin_sick_cor, axis=0)
+	wt_cor, _ = get_corr_im_dir(test_wt)
+	pcd35_cor, _ = get_corr_im_dir(pcd35)
+	pcd36_cor, _ = get_corr_im_dir(pcd36)
+	pcd38_cor, _ = get_corr_im_dir(pcd38)
+
+
+
+	np.save("wt", wt_cor)
+	np.save("pcd35", pcd35_cor)
+	np.save("pcd36", pcd36_cor)
+	np.save("pcd38", pcd38_cor)
+
 	
-	for dirc in spin_healthy:
-		corrs, _ = get_corr_im_dir(dirc)
-		spin_heal_cor.append(corrs)
-	spin_heal_cor = np.concatenate(spin_heal_cor, axis=0)
-		
-
-
-
-	np.save("spin_healthy", spin_heal_cor)
-	np.save("spin_sick", spin_sick_cor)
-
-	np.save("wide_healthy", wide_heal_cor)
-	np.save("wide_sick", wide_sick_cor)
-
 		
 
 
