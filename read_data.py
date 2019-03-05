1e3853e716a0823b0d3d7068722f6ce13151c879
eliaswound <eliaswound@gmail.com>
2019-03-05 14:35:23 -0500

    Update read_data.py
    PCD40 and WT for PCD40, Elias and Zhen, PCD40E and PCD40Z added. Data not loaded to server

--- read_data.py
+++ read_data.py
@@ -36,25 +36,34 @@ if (__name__ == "__main__"):
 	test_wt = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/WT_cropped'
 	wt_17 = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/WT_PCD17'
 	wt_23 = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/WT_PCD23'
+	wt_40E = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/WT_PCD40E'
+	wt_40Z = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/WT_PCD40Z'
 	pcd36 = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/PCD36_cropped'
 	pcd17 = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/PCD17_cropped'
 	pcd23 = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/PCD23_cropped'
+	pcd40 = '/hpf/largeprojects/agoldenb/yasaman/Mennella_Lab/PCD40_cropped'
 	
 	wt_cor, _ = get_corr_im_dir(test_wt)
 	wt17_cor, _ = get_corr_im_dir(wt_17)
 	wt23_cor, _ = get_corr_im_dir(wt_23)
+	wt40E_cor, _ = get_corr_im_dir(wt_40E)
+	wt40Z_cor, _ = get_corr_im_dir(wt_40Z)
 	pcd36_cor, _ = get_corr_im_dir(pcd36)
 	pcd17_cor, _ = get_corr_im_dir(pcd17)
 	pcd23_cor, _ = get_corr_im_dir(pcd23)
+	pcd40_cor, _ = get_corr_im_dir(pcd40)
 
 
 
 	np.save("wt", wt_cor)
 	np.save("wt17", wt17_cor)
 	np.save("wt23", wt23_cor)
+	np.save("wt40E", wt40E_cor)
+	np.save("wt40Z", wt40Z_cor)
 	np.save("pcd17", pcd17_cor)
 	np.save("pcd36", pcd36_cor)
 	np.save("pcd23", pcd23_cor)
+	np.save("pcd40", pcd40_cor)
 
 	
 		
